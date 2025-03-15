5daf579030d18af8162145fe1eba265796375808
Mark Daemon <mark_tp_smith@protonmail.com>
2025-03-15 22:22:55 +0000

    Latest week notes post.

--- src/generator/util.clj
+++ src/generator/util.clj
@@ -5,27 +5,13 @@
            (java.util TimeZone)))
 
 (def publish-folder "docs/blog/")
-(def blog-path "/blog/")
-(def landing-recent-posts-count 3)
-
-(defn- escape-special-chars
-  "Escapes special characters in the given string"
-  [string]
-  (-> string
-      (str/replace "{" "\\{")
-      (str/replace "}" "\\}")))
-
-(defn create-pattern-line-with
-  "Creates a pattern line with the given pattern"
-  [string]
-  (re-pattern (str "(?m)^.*" (escape-special-chars string) ".*$\n?")))
 
 (defn create-date-formatter
   "Create the style of date formatter to be consistently used across the app"
   []
   (doto (SimpleDateFormat. "yyyy-MM-dd")
     (.setTimeZone (TimeZone/getTimeZone "UTC"))))
-    
+
 (defn create-RFC-822-date-formatter
   "Create the style of date formatter to be consistently used across the app"
   []
