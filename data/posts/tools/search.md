e75964c94b161a4ed124ba2935065ad150c73236
Mark Daemon <mark_tp_smith@protonmail.com>
2025-04-27 18:27:18 +0100

    Integrate search into the site.

---
+++ data/posts/tools/search.md
@@ -0,0 +1,8 @@
+title: Search 
+date: 2025-04-27
+excerpt: Search this blog!
+section: tools
+scan-time: 20s
+---
+
+{{ (apply-section "data/templates/search.html") }}
--- src/generator/generate_search.clj
+++ src/generator/generate_search.clj
@@ -11,8 +11,9 @@
 
 (def stop-words
   (set ["", "the", "when", "if" "and" "or" "but" "is" "are" "was" "were"
-        "a" "an" "the" "this" "that" "it" "its" "in" "on" "at"
-        "for" "to" "of" "with" "by" "from"]))
+        "a" "an" "the" "this" "that" "it" "its" "it's" "in" "on" "at"
+        "for" "to" "of" "with" "by" "from" "his" "her" "their" "my" "i", "he"
+        "re"]))
 
 (defn- tokenise-post
   "Tokenises the post content into words"
@@ -21,7 +22,8 @@
             (re-seq #"\w+")
             (map str/lower-case)
             (map str/trim)
-            (filter #(not (contains? stop-words %))))))
+            (filter #(not (contains? stop-words %)))
+            (filter #(> (count %) 3)))))
 
 (defn- parse-search-entry
   "Maps the post data to a search entry"
@@ -48,10 +50,24 @@
         (assoc :last_id id)
         (assoc-in [:_ids id] index-entry))))
 
+(defn- generate-prefix-terms
+  "Generate the prefix terms for the search index. It takes each terms and
+  adds a matching prefix for characters 2 or more"
+  [terms [key value]]
+  {:pre [(s/valid? map? terms)]}
+  (if (keyword? key)
+    (assoc terms key value)
+    (let [prefixes (->> (map #(subs key 0 %) (range 2 (inc (count key)))))
+          prefixes-map (reduce #(assoc-in %1 [:_prefixes %2] (conj (get-in %1 [:_prefixes %2] []) key)) terms prefixes)]
+      (assoc prefixes-map key value))))
+
 (defn generate_search_index!
   "Generates the search page for the site and stores it in the output directory"
   [blog]
   {:pre [(s/valid? ::model/TemplateBlog blog)]}
-  (let [posts (flatten (map :posts (:sections blog)))
-        index (reduce create-post-index {} posts)]
-    (spit (str util/publish-folder "search.json") (generate-string (dissoc index :last_id)))))
+  (let [posts (apply concat (map :posts (filter #(not (:is-home %)) (:sections blog))))
+        index (->> posts
+                   (reduce create-post-index {})
+                   (reduce generate-prefix-terms {}))
+        index (dissoc index :last_id)]
+    (spit (str util/publish-folder "search.json") (generate-string index))))
